41294665b0db04ba057a711fd4d59f9b8f48ca66
Tejas Shah <tejasshah2k19@gmail.com>
2025-02-01 20:53:09 +0530

    double ll

---
+++ DoubleLinkedList.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+} *head = NULL, *last = NULL;
+
+void addNode(int num)
+{
+    struct node *tmp;
+    if (head == NULL)
+    {
+        head = malloc(sizeof(struct node));
+        head->data = num;
+        head->next = NULL;
+        head->prev = NULL;
+        last = head;
+    }
+    else
+    {
+        tmp = malloc(sizeof(struct node));
+        tmp->data = num;
+        tmp->next = NULL;
+        last->next = tmp;
+        tmp->prev = last;
+        last = tmp;
+    }
+}
+
+void display()
+{
+    struct node *p;
+    p = head;
+    while (p != NULL)
+    {
+        printf(" %d", p->data);
+        p = p->next;
+    }
+}
+int main()
+{
+    int ch;
+    int num;
+    while (1)
+    {
+        printf("\n0 For Exit\n1 For Add Node\n2 For Display\nEnter choice");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 0:
+            exit(0);
+        case 1:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNode(num);
+            break;
+        case 2:
+            display();
+            break;
+
+        default:
+            break;
+        }
+    }
+    return 0;
+}
